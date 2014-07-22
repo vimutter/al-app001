c58eb74b4590bf6a666803e63fc2e7b422437eb7
Mark Guk <mark.guk.e@gmail.com>
2014-07-22 22:24:14 +0300

    Made gadgets_controller green

--- app/controllers/gadgets_controller.rb
+++ app/controllers/gadgets_controller.rb
@@ -1,2 +1,8 @@
 class GadgetsController < ApplicationController
+  TRUE = ['1', 'true', true]
+
+  def index
+    @gadgets = Gadget.for_user(current_user).page(params[:page]).per(10)
+    @panel = TRUE.include?(params[:panel])
+  end
 end
--- spec/controllers/gadgets_controller_spec.rb
+++ spec/controllers/gadgets_controller_spec.rb
@@ -8,30 +8,31 @@ RSpec.describe GadgetsController, :type => :controller do
 
     before(:each) do
       allow(controller).to receive(:current_user).and_return user
-      expect(Gadget).to receive(:for_user).with(user).and_return [gadget]
+      allow(controller).to receive(:authenticate_user!)
+      allow(Gadget).to receive_message_chain('for_user.page.per').and_return [gadget]
     end
 
     it 'should render all users gadgets in list view' do
       get :index
 
-      expect(assigns[:gadgets]).to eql [gadgets]
+      expect(assigns[:gadgets]).to eql [gadget]
     end
 
     it 'should render all users gadgets in panels view' do
       get :index, panel: true
 
-      expect(assigns[:panel]).to be_true
+      expect(assigns[:panel]).to eql(true)
     end
 
     it 'should support pagination' do
       chain = double(:gadgets)
       expect(Gadget).to receive(:for_user).with(user).and_return chain
-      expect(chain).to receive(:page).with(2).and_return chain
+      expect(chain).to receive(:page).with('2').and_return chain
       expect(chain).to receive(:per).with(10).and_return [gadget]
 
       get :index, page: 2
 
-      expect(assigns[:gadgets]).to eql [gadgets]
+      expect(assigns[:gadgets]).to eql [gadget]
     end
   end
 end
